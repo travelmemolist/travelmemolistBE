ef0cdcd25d5d9844df3c6e7f8e76250b04c87f19
phamnguyentruongtin <truongtin1206@gmail.com>
2024-02-26 07:26:22 +0700

    update

---
+++ travelmemolistBE/src/main/java/com/example/travelmemolistbe/TravelmemolistBeApplication.java
@@ -0,0 +1,15 @@
+package com.example.travelmemolistbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmemolistBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelmemolistBeApplication.class, args);
+        System.out.println("hello");
+    }
+
+
+}
---
+++ travelmemolistBE/src/test/java/com/example/travelmemolistbe/TravelmemolistBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelmemolistbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmemolistBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
