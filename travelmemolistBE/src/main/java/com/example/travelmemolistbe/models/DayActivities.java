c8891f3a481136e39c13582df1ccbe00a0ba683f
phamnguyentruongtin <truongtin1206@gmail.com>
2024-02-28 21:58:36 +0700

    up

--- travelmemolistBE/src/main/java/com/example/travelmemolistbe/models/DayActivities.java
+++ travelmemolistBE/src/main/java/com/example/travelmemolistbe/models/DayActivities.java
@@ -3,6 +3,7 @@ package com.example.travelmemolistbe.models;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @Entity
@@ -10,6 +11,8 @@ public class DayActivities {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDayActivities;
+    private String day;
+    private Date currentDay;
     private String nameOfDayActivities;
     @ManyToOne(fetch = FetchType.LAZY)
     private Schedules schedule;
