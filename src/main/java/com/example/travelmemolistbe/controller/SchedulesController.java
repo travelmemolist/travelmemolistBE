2095ab464e7a15b04665fe704f190a60a8c1a3c6
PhatVN <vonguyenphat012@gmail.com>
2024-02-29 00:57:57 +0700

    up

--- src/main/java/com/example/travelmemolistbe/controller/SchedulesController.java
+++ src/main/java/com/example/travelmemolistbe/controller/SchedulesController.java
@@ -1,15 +1,13 @@
 package com.example.travelmemolistbe.controller;
 
 import com.example.travelmemolistbe.models.Schedules;
-import com.example.travelmemolistbe.service.ISchedulesService;
+import com.example.travelmemolistbe.service.impl.SchedulesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,14 +15,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/schedules")
 public class SchedulesController {
     @Autowired
-    private ISchedulesService ischedulesService;
+    private SchedulesService iSchedulesService;
 
     @GetMapping("")
     public ResponseEntity<Page<Schedules>> findAllSchedules(@RequestParam(value = "title") String title,
                                                             @RequestParam(value = "userid") String userid,
                                                             @RequestParam(defaultValue = "1") int page) {
         Pageable pageable = PageRequest.of(page - 1, 5);
-        Page<Schedules> pageSchedules = ischedulesService.findAllSchedulesByUserId(pageable, userid, title);
+        Page<Schedules> pageSchedules = iSchedulesService.findAllSchedulesByUserId(pageable, userid, title);
         if (pageSchedules.getContent() == null) {
             ResponseEntity<Schedules> responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
