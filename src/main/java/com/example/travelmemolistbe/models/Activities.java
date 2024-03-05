96e97239f9fd8f41f47b930265ac1f50c63cb61e
PhatVN <vonguyenphat012@gmail.com>
2024-03-05 15:27:33 +0700

    update

--- src/main/java/com/example/travelmemolistbe/models/Activities.java
+++ src/main/java/com/example/travelmemolistbe/models/Activities.java
@@ -17,6 +17,8 @@ public class Activities {
     private String activityName;
     private String description;
     private Boolean isDeleted;
+    private String startTime;
+    private String endTime;
     private Boolean stateActive;
     @ManyToOne(fetch = FetchType.LAZY)
     private DayActivities dayActivities;
