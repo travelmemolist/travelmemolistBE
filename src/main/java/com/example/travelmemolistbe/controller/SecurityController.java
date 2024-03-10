f70f1d788212b9f16da3b23a883bfe9bb7203104
PhatVN <vonguyenphat012@gmail.com>
2024-03-11 02:14:21 +0700

    update

--- src/main/java/com/example/travelmemolistbe/controller/SecurityController.java
+++ src/main/java/com/example/travelmemolistbe/controller/SecurityController.java
@@ -1,8 +1,11 @@
 package com.example.travelmemolistbe.controller;
 
+import com.example.travelmemolistbe.dto.AccountInfoDTO;
 import com.example.travelmemolistbe.dto.LoginRequest;
 import com.example.travelmemolistbe.dto.LoginResponse;
+import com.example.travelmemolistbe.dto.ResponseStatusDTO;
 import com.example.travelmemolistbe.jwt.JwtTokenProvider;
+import com.example.travelmemolistbe.models.Activities;
 import com.example.travelmemolistbe.models.User;
 import com.example.travelmemolistbe.repository.IUserRepository;
 import com.example.travelmemolistbe.service.impl.AccountService;
@@ -13,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +24,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class SecurityController {
     @Autowired
     AuthenticationManager authenticationManager;
@@ -32,29 +37,52 @@ public class SecurityController {
 
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginRequest.getUsername(),
-                        loginRequest.getPassword()
-                ));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = tokenProvider.generateToken((CustomUserDetails) authentication.getPrincipal());
-        String username = tokenProvider.getUserNameFromJWT(jwt);
-        return ResponseEntity.ok( new LoginResponse(jwt,username));
+    public ResponseEntity<ResponseStatusDTO> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+//        Authentication authentication = authenticationManager.authenticate(
+//                new UsernamePasswordAuthenticationToken(
+//                        loginRequest.getUsername(),
+//                        loginRequest.getPassword()
+//                ));
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        String jwt = tokenProvider.generateToken((CustomUserDetails) authentication.getPrincipal());
+//        String username = tokenProvider.getUserNameFromJWT(jwt);
+//        AccountInfoDTO accountInfoDTO = userRepository.getUserByUsername(username);
+//        LoginResponse loginResponse = new LoginResponse(jwt,accountInfoDTO);
+//        return new  ResponseEntity<>(new ResponseStatusDTO<>(200,"Đăng nhập thành công",loginResponse),HttpStatus.OK);
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginRequest.getUsername(),
+                            loginRequest.getPassword()
+                    ));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = tokenProvider.generateToken((CustomUserDetails) authentication.getPrincipal());
+            String username = tokenProvider.getUserNameFromJWT(jwt);
+            AccountInfoDTO accountInfoDTO = userRepository.getUserByUsername(username);
+            LoginResponse loginResponse = new LoginResponse(jwt, accountInfoDTO);
+            return new ResponseEntity<>(new ResponseStatusDTO<>(200, "Đăng nhập thành công", loginResponse), HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>(new ResponseStatusDTO<>(401, "Đăng nhập thất bại", null), HttpStatus.UNAUTHORIZED);
+        }
     }
     @GetMapping("/test")
     public ResponseEntity<String> test(){
         String test = "hello";
         return ResponseEntity.ok(test);
     }
-    @PostMapping("/user/create")
-    public ResponseEntity<?> createAccount(@RequestBody User user){
+    @PostMapping("/register")
+    public ResponseEntity<ResponseStatusDTO> createAccount(@RequestBody User user){
         User  user1 = userRepository.findUserByUsername(user.getUsername());
+        if (user.getUsername() == null){
+            return new ResponseEntity<>(new ResponseStatusDTO(409,"Username null",""),HttpStatus.OK);
+        }
+        if (user.getPassword() == null){
+            return new ResponseEntity<>(new ResponseStatusDTO(409,"Password null", ""),HttpStatus.OK);
+        }
         if (user1!=null){
-            return new ResponseEntity<>("User exits", HttpStatus.CONFLICT);
+            return new ResponseEntity<>(new ResponseStatusDTO(409,"Tên đăng nhập tồn tại",""), HttpStatus.OK);
         }
         accountService.createUser(user);
-        return new ResponseEntity<>("Create successfully",HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseStatusDTO(200,"Tạo tài khoảng thành công",""),HttpStatus.OK);
     }
 }
